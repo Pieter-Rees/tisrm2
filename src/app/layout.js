c89b42ae76014ba803a5f6be57d1bb0d697e9057
Pieter Rees <pieter@pieterrees.nl>
2024-04-11 18:25:56 +0200

    init chakra

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Header from "@/components/header/page";
 import Footer from "@/components/footer/page";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
