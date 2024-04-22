631ad17becd4818bcae7b2eb0bce765c521eee3d
Pieter Rees <pieter@pieterrees.nl>
2024-04-22 18:15:47 +0200

    fixed

--- src/components/header/page.js
+++ src/components/header/page.js
@@ -1,10 +1,13 @@
 import { Box, Heading } from "@chakra-ui/react";
+import Logo from "@/components/logo";
+import Navbar from "@/components/navbar/page";
 
 export default function Header() {
     return (
         <Box bg="brand.100" py={4} px={8}>
-            <Heading color="white" size="lg">
-                Header
+            <Heading color="white" size="lg" display='flex' justifyContent='space-between'>
+                <Logo />
+                <Navbar />
             </Heading>
         </Box>
     );
---
+++ src/components/logo.js
@@ -0,0 +1,10 @@
+
+export default function Logo() {
+
+    return (
+        <>
+            TISRM.nl
+        </>
+    )
+
+}
