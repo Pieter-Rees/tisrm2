308a8c73a52b24f8235cd8c0ec27e086a38485cb
Pieter Rees <pieter@pieterrees.nl>
2024-05-31 17:10:52 +0200

    footer work

--- src/components/footer/page.js
+++ src/components/footer/page.js
@@ -1,33 +1,72 @@
-import { Box, Text, SimpleGrid, Container } from "@chakra-ui/react";
+import {
+    ListItem,
+    ListIcon,
+    Divider,
+    UnorderedList, Box, Flex, Text, SimpleGrid, Container, Heading
+} from "@chakra-ui/react";
 import { contactInfo, currentYear } from "@/data/general";
 import FooterLogos from "@/components/footer-logos";
+import Link from 'next/link'
 
 export default function Footer() {
     return (
         <>
             <FooterLogos />
 
-            <Box bg="gray.200" mt={8}>
-                <Container maxW='2xl'>
+            <Flex bg="gray.200" paddingY='16' alignItems='center' >
+                <Container maxW='full' >
                     <SimpleGrid minChildWidth='120px' spacing='40px'>
                         <Box>
-                            <Text textAlign="center" color="gray.500">
+                            <Heading as='h3' variant='md'>
                                 {contactInfo.name}
+                            </Heading>
+                            <Text color="gray.500">
+                                Het verdient aanbeveling dat verzekeraars een polis uitbrengen die de gevolgen dekt van een niet geheel begrepen verzekering!
                             </Text>
                         </Box>
                         <Box>
-                            <Text textAlign="center" color="gray.500">
-                                {contactInfo.name}
-
-                            </Text>
+                            <Heading as='h3' variant='md'>
+                                Navigatie
+                            </Heading>
+                            <Flex flexDir='column'>
+                                <Link href="/">Home</Link>
+                                <Link href="/verzekeringen">Verzekeringen</Link>
+                                <Link href="/taxi">Taxi</Link>
+                                <Link href="/risk-management">Risk management</Link>
+                                <Link href="/over-ons">Over ons</Link>
+                                <Link href="/bestanden">Bestanden</Link>
+                                <Link href="/contact">Contact</Link>
+                            </Flex>
                         </Box>
                         <Box>
-                            <Text textAlign="center" color="gray.500">
-                                {contactInfo.address}
-                                {contactInfo.email}
-                                {contactInfo.phone}
+                            <Heading as='h3' variant='md'>
+                                Contact informatie
+                            </Heading>
 
-                            </Text>
+                            <SimpleGrid minChildWidth='120px' spacing='40px'>
+                                <Box>
+                                    <UnorderedList>
+                                        <ListItem>{contactInfo.address}</ListItem>
+                                        <ListItem>{contactInfo.email}</ListItem>
+                                        <ListItem>{contactInfo.city}</ListItem>
+                                    </UnorderedList>
+                                </Box>
+                                <Box>
+                                    <UnorderedList>
+                                        <ListItem>{contactInfo.postalBox}</ListItem>
+                                        <ListItem>{contactInfo.postalCode}</ListItem>
+                                        <ListItem>{contactInfo.city}</ListItem>
+                                    </UnorderedList>
+                                </Box>
+                            </SimpleGrid>
+                            <Divider orientation='horizontal' />
+                            <Box>
+                                <UnorderedList>
+                                    <ListItem>{contactInfo.phone}</ListItem>
+                                    <ListItem>{contactInfo.email}</ListItem>
+                                    <ListItem>{contactInfo.linkedIn}</ListItem>
+                                </UnorderedList>
+                            </Box>
                         </Box>
                     </SimpleGrid>
                     <Box >
@@ -36,7 +75,7 @@ export default function Footer() {
                         </Text>
                     </Box>
                 </Container>
-            </Box>
+            </Flex>
         </>
 
     )
