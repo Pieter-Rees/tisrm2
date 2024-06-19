06415e88b0f6030dd8dba90226f390b904934012
Pieter Rees <pieter@pieterrees.nl>
2024-06-19 16:01:44 +0200

    test form

--- src/app/offerte/form.js
+++ src/app/offerte/form.js
@@ -1,65 +1,88 @@
-import { useForm } from "react-hook-form";
 import React from "react";
 import { FormErrorMessage, FormLabel, FormControl, Input, Button } from "@chakra-ui/react";
+import { useForm } from 'react-hook-form'
+import { Flex } from "@chakra-ui/react";
 
-export default function RegistrationForm({ onRegistered }) {
+export default function RegistrationForm() {
     const {
-        handleSubmit, // handels the form submit event
-        register, // ties the inputs to react-form
-        formState: { errors, isSubmitting }, // gets errors and "loading" state
-    } = useForm();
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+    } = useForm()
 
+    function onSubmit(values) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                resolve()
+            }, 3000)
+        })
+    }
     return (
-        <form onSubmit={handleSubmit(onRegistered)} noValidate>
-            {/* noValidate will stop the browser validation, so we can write our own designs and logic */}
-            <FormControl isInvalid={errors.enrollmentId}>
-                <FormLabel htmlFor="enrollmentId">
-                    Enrollment ID
-                    {/* the form label from chakra ui is tied to the input via the htmlFor attribute */}
-                </FormLabel>
-                {/* you should use the save value for the id and the property name */}
-                <Input
-                    id="enrollmentId"
-                    placeholder="AX-3345-GHBB-2021"
-                    {
-                    ...register("enrollmentId", {
-                        required: "Don't forget the Enrollment ID",
-                    }) /* this register function will take care of the react-form binding to the ui */
-                    }
-                ></Input>
-                {/* react-form will calculate the errors on submit or on dirty state */}
-                <FormErrorMessage>{errors.enrollmentId && errors.enrollmentId.message}</FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.name}>
-                <FormLabel required htmlFor="name">
-                    Name
-                </FormLabel>
-                <Input
-                    id="name"
-                    placeholder="John Coolio"
-                    {...register("name", {
-                        required: "Did you forget your name?",
-                    })}
-                ></Input>
-                <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.email}>
-                <FormLabel required htmlFor="email">
-                    E-Mail Address
-                </FormLabel>
-                <Input
-                    id="email"
-                    placeholder="john.coolio@events.de"
-                    type="email"
-                    {...register("email", {
-                        required: "We really need your E-Mail!",
-                    })}
-                ></Input>
-                <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
-            </FormControl>
-            <Button mt={10} colorScheme="blue" isLoading={isSubmitting} type="submit">
-                Register 🐱‍🏍
-            </Button>
-        </form>
+        <Flex width='full'>
+            <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%;' }}>
+                <FormControl isInvalid={errors.onderneming}>
+                    <FormLabel htmlFor='name'>Naam onderneming</FormLabel>
+                    <Input
+                        id='onderneming'
+                        {...register('onderneming', {
+                            required: 'This is required',
+                            minLength: { value: 4, message: 'Minimum length should be 4' },
+                        })}
+                    />
+                    <FormErrorMessage>
+                        {errors.onderneming && errors.onderneming.message}
+                    </FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={errors.name}>
+                    <FormLabel htmlFor='name'>Uw naam</FormLabel>
+                    <Input
+                        id='name'
+                        {...register('name', {
+                            required: 'This is required',
+                            minLength: { value: 4, message: 'Minimum length should be 4' },
+                        })}
+                    />
+                    <FormErrorMessage>
+                        {errors.name && errors.name.message}
+                    </FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={errors.email}>
+                    <FormLabel htmlFor='email'>Uw email adres</FormLabel>
+                    <Input
+                        id='email'
+                        {...register('email', {
+                            required: 'This is required',
+                            minLength: { value: 4, message: 'Minimum length should be 4' },
+                        })}
+                    />
+                    <FormErrorMessage>
+                        {errors.email && errors.email.message}
+                    </FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={errors.meldcode}>
+                    <FormLabel htmlFor='meldcode'>Uw meldcode</FormLabel>
+                    <Input
+                        id='meldcode'
+                        {...register('meldcode', {
+                            required: 'This is required',
+                            minLength: { value: 4, message: 'Minimum length should be 4' },
+                        })}
+                    />
+                    <FormErrorMessage>
+                        {errors.meldcode && errors.meldcode.message}
+                    </FormErrorMessage>
+                </FormControl>
+
+
+                <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                    Submit
+                </Button>
+            </form>
+        </Flex>
+
     );
 }
